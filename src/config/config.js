927c9a17dfbbabe438f0412036486989da925577
kazukimuta <kazukimuta@gmail.com>
2019-01-05 00:25:49 +0900

    netlify hosting

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   blogTitle: 'wakuwaku blog',
   blogDescription: 'Webブログラミング, 機械学習, 趣味のことなど',
-  blogUrl: 'https://XXX',
+  blogUrl: 'https://kazukimuta.netlify.com/',
   blogAuthor: 'kazukimuta',
   blogKeyword: ['Webプログラミング', '機械学習'],
   githubPage: 'https://github.com/kazukimuta',
